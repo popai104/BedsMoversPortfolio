b06fb46084416cbb77e32e7e40c31c6c2375192f
matteo <kippermint6@gmail.com>
2024-05-31 14:53:18 +0200

    skeleton ui setup

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script>
+</script>
+
+
+<main>
+<body data-theme="crimson">
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</body>
+</main>
+
+
+<style>
+</style>
