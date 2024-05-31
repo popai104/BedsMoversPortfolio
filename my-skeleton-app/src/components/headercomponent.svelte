79cc476e2541e4aa8568cfb8b6614057bc4ca245
matteo <kippermint6@gmail.com>
2024-05-31 21:54:08 +0200

    cleanup and setup

---
+++ my-skeleton-app/src/components/headercomponent.svelte
@@ -0,0 +1,19 @@
+<script>
+    import '../app.postcss';
+	import { AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<main>
+    <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+        <svelte:fragment slot="lead">
+            <strong class="text-xl uppercase">bedsmovers</strong>
+        </svelte:fragment>
+        <svelte:fragment slot="default">
+        </svelte:fragment>
+        <svelte:fragment slot="trail">
+        </svelte:fragment>
+    </AppBar>
+</main>
+
+<style>
+</style>
---
+++ my-skeleton-app/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import Header from '../components/headercomponent.svelte' ;
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<Header />
+	</svelte:fragment>	
+	<slot />
+</AppShell>
